14caa3bf4495be2078b1f12e6012475b09ba0e27
danielsousast <daniel.sousa.developer@gmail.com>
2023-12-27 14:08:03 -0300

    feat: crete container

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,14 @@
+import { renderWithTheme } from '../../utils/tests/helpers'
+import { Container } from '.'
+
+describe('<Container />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Won Games</span>
+      </Container>
+    )
+    expect(container.firstChild).toHaveStyleRule('max-width', '130rem')
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
